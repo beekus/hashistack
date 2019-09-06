e847e48470aa68051c8f444f7f5659ae8a4d9c60
Michael Nomitch <mike@Michaels-MacBook-Pro.local>
2019-09-06 11:36:39 -0500

    config updates

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
 provider "aws" {
-  region  = var.region
+  region = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
